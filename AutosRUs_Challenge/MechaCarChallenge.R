835d726797269effa04f7b86774b1d0651491202
Alan Luo Fanng <afanng24@gmail.com>
2021-08-02 09:05:16 -0400

    Add files via upload

--- AutosRUs_Challenge/MechaCarChallenge.R
+++ AutosRUs_Challenge/MechaCarChallenge.R
@@ -1,3 +1,9 @@
 #Challenge AutosRUs File
 
 
+#Deliverable 1
+library(dplyr)
+library(tidyverse)
+mecha_df<- read.csv(file='MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data=mecha_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data=mecha_df))
