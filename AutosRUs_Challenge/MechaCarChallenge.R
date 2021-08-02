0376dfbeb6535e56947148d75f39fa764b104958
Alan Luo Fanng <afanng24@gmail.com>
2021-08-02 09:55:57 -0400

    Add files via upload

--- AutosRUs_Challenge/MechaCarChallenge.R
+++ AutosRUs_Challenge/MechaCarChallenge.R
@@ -1,9 +1,29 @@
 #Challenge AutosRUs File
 
 
-#Deliverable 1
+# Deliverable 1
 library(dplyr)
 library(tidyverse)
 mecha_df<- read.csv(file='MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data=mecha_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data=mecha_df))
+
+
+# Deliverable 2
+
+coil_df<- read.csv(file="Suspension_Coil.csv", check.names = F,stringsAsFactors = F)
+total_summary<- coil_df %>% summarise(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+
+
+
+
+
+
+
+
+
+
+
